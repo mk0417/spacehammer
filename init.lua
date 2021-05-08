c6830c93b0334e71dd04df76cf171cf9ec2013ec
“mk0417” <pengdata1@gmail.com>
2021-05-08 15:35:09 +0100

    fix luarocks path

--- init.lua
+++ init.lua
@@ -10,6 +10,17 @@
 --  License: MIT
 --
 
+local lVer = _VERSION:match("Lua (.+)$")
+-- specify luarocks path yourself if this doesn't find it in the normal places
+local luarocks = "/usr/local/bin/luarocks"
+if #luarocks > 0 then
+    package.path = package.path .. ";" .. hs.execute(
+            luarocks .. " --lua-version " .. lVer .. " path --lr-path"
+        ):gsub("\n", "")
+    package.cpath = package.cpath .. ";" .. hs.execute(
+            luarocks .. " --lua-version " .. lVer .. " path --lr-cpath"
+        ):gsub("\n", "")
+end
 
 hs.alert.show("Spacehammer config loaded")
 
