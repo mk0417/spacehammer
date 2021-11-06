20c4bf8b326d8f0e4240fce492a751bacc06e587
“mk0417” <pengdata1@gmail.com>
2021-11-06 13:03:22 +0000

    change volume

--- init.lua
+++ init.lua
@@ -10,3 +10,23 @@ fennel = require("fennel")
 table.insert(package.loaders or package.searchers, fennel.searcher)
 
 require "core"
+
+
+local super = {"alt", "cmd"}
+
+-- Volume keys
+local function changeVolume(diff)
+    return function()
+        local current = hs.audiodevice.defaultOutputDevice():volume()
+        local new = math.min(100, math.max(0, math.floor(current + diff)))
+        if new > 0 then
+            hs.audiodevice.defaultOutputDevice():setMuted(false)
+        end
+        hs.alert.closeAll(0.0)
+        hs.alert.show("Volume " .. new .. "%", {}, 0.5)
+        hs.audiodevice.defaultOutputDevice():setVolume(new)
+    end
+end
+
+hs.hotkey.bind(super, 'up', changeVolume(3))
+hs.hotkey.bind(super, 'down', changeVolume(-3))
